54d18a100ee888a33ee844810ab0c01988ba47cb
Jimmy Lin <usmcjimmy@gmail.com>
2019-03-29 14:08:18 -0400

    able to render 1 item

--- client/src/components/legal/Shipping.js
+++ client/src/components/legal/Shipping.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react'
 
 import './_legal.scss'
 
@@ -6,7 +7,64 @@ class Shipping extends Component {
   render() {
     return (
       <div id="shipping">
-        <h1>Shipping</h1>
+        <h3>Shipping within United States</h3>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Order Value</Table.HeaderCell>
+              <Table.HeaderCell>Standard / USPS</Table.HeaderCell>
+              <Table.HeaderCell>Express</Table.HeaderCell>
+              <Table.HeaderCell>Rush</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell></Table.Cell>
+              <Table.Cell>5-9 business days</Table.Cell>
+              <Table.Cell>2-3 business days</Table.Cell>
+              <Table.Cell>1-2 business days</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>up to $34.99</Table.Cell>
+              <Table.Cell>$2.95</Table.Cell>
+              <Table.Cell>$14.95</Table.Cell>
+              <Table.Cell>$21.95</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>$35 + </Table.Cell>
+              <Table.Cell style={{color: "red"}}>FREE</Table.Cell>
+              <Table.Cell>$14.95</Table.Cell>
+              <Table.Cell>$21.95</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <p>Orders over $35 will not be charged the standard shipping fee, these rartes are not applicable for orders shipped outside of the US</p>
+        <h3>Shipping to Canada, Mexico and Rest of the World</h3>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell></Table.HeaderCell>
+              <Table.HeaderCell>Canada & Mexico</Table.HeaderCell>
+              <Table.HeaderCell>Rest of the World</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Stadard Shipping</Table.Cell>
+              <Table.Cell>$14.95</Table.Cell>
+              <Table.Cell>$24.95</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <p>Orders shipped outside of the contiguous US and to PO boxes are shipped via USPS Priority Mail and cannot be expedited.</p>
+        <p>Please note that our delivery options refer only to the delivery time frame and not to the time required to prepare your order.  While most orders are ready for shipping within 1 day of ordering, some orders may take additional time.</p>
+
+        <h3>Shipping within United States</h3>
+        <p>Our policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can’t offer you a refund or exchange.</p>
+        <p>To be eligible for a return, your item must be unused and in the same condition that you received it. It must also be in the original packaging.</p>
+        <p>Visit <a href="/#" style={{fontWeight: "bold"}}>Return & Exchanges</a> for more information on completing a return.</p>
       </div>
     )
   }
--- client/src/components/shops/product/Product.js
+++ client/src/components/shops/product/Product.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchOneProduct } from '../../../actions';
-import { Container, Grid, Image, Select, Button, Icon } from 'semantic-ui-react'
+import { Container, Grid, Image, Select, Button, Icon, Divider, Rating, Accordion } from 'semantic-ui-react'
 
+import Shipping from '../../legal/Shipping'
 import './_product.scss'
 
 const QtyOptions = [
@@ -22,14 +23,18 @@ const QtyOptions = [
 class Product extends Component {
   state = {
     product: {},
+    activeIndex: "",
+    xsmallActive: false,
     smallActive: false,
     mediumActive: false,
     largeActive: false,
     xlargeActive: false,
+    xxlargeActive: false,
     checked: false,
     selectedSize: "",
     color: "",
     colorOptions: [],
+    sizesContainer: [],
     qty: ""
   }
 
@@ -40,26 +45,65 @@ class Product extends Component {
     await this.setState({
       product: this.props.product
     })
-    console.log('this.state', this.state.product)
-    this.renderColorOptions()
+    console.log('One Product -----', this.state.product)
+    await this.renderColorOptions()
+    this.renderSizeOptions()
   }
 
   toggle = () => this.setState({ checked: !this.state.checked })
 
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  renderSizeOptions(){
+    if (this.state.product.options.length > 1) {
+      if( typeof this.state.product.options[1].values !== "undefined") {
+
+      } else {
+        const sizes = this.state.product.options[1].values;
+        if(sizes.includes("XS")){
+          this.state.sizesContainer.push({size: 0, name: "XS", type: "xsmallActive"})
+        }
+        if(sizes.includes("S")){
+          this.state.sizesContainer.push({size: 1, name: "S", type: "smallActive"})
+        }
+        if(sizes.includes("M")){
+          this.state.sizesContainer.push({size: 2, name: "M", type: "mediumActive"})
+        }
+        if(sizes.includes("L")){
+          this.state.sizesContainer.push({size: 3, name: "L", type: "largeActive"})
+        }
+        if(sizes.includes("XL")){
+          this.state.sizesContainer.push({size: 4, name: "XL", type: "xlargeActive"})
+        }
+        if(sizes.includes("2XL")){
+          this.state.sizesContainer.push({size: 5, name: "2XL", type: "xxlargeActive"})
+        }
+      }
+    }
+
+    console.log("sizesContainer----", this.state.sizesContainer)
+  }
+
   renderColorOptions(){
     // this.setState({ colorOptions: this.state.product.options[0].values })
     const colors = this.state.product.options[0].values;
-    console.log('colors', colors)
+    // console.log('colors', colors)
     colors.map((e) => {
-      console.log('e', e)
+      // console.log('e', e)
       const data = {
         key: e,
         text: e,
         value: e
       }
-      this.state.colorOptions.push(data)
+      return this.state.colorOptions.push(data)
     })
-    console.log('this.state.colorOptions', this.state.colorOptions)
+    // console.log('this.state.colorOptions', this.state.colorOptions)
   }
 
   renderingLeftColumn(){
@@ -84,68 +128,143 @@ class Product extends Component {
   handleSizeChange(size){
     switch(size) {
       case 0:
-        return this.setState({ selectedSize: "Small" });
+        return this.setState({ selectedSize: "XS" });
       case 1:
-          return this.setState({ selectedSize: "Medium" });
+        return this.setState({ selectedSize: "S" });
       case 2:
-        return this.setState({ selectedSize: "Large" });
+          return this.setState({ selectedSize: "M" });
       case 3:
-        return this.setState({ selectedSize: "Xlarge" });
+        return this.setState({ selectedSize: "L" });
+      case 4:
+        return this.setState({ selectedSize: "XL" });
+      case 5:
+        return this.setState({ selectedSize: "2XL" });
       default:
         return ""
     }
   }
+  handleSetXSmallActive(e) {
+    const size = e.target.value
+    this.setState({ xsmallActive: !this.state.xsmallActive, smallActive: false, mediumActive: false, largeActive: false, xlargeActive: false, xxlargeActive: false })
+    this.handleSizeChange(size)
+  }
 
   handleSetSmallActive(e) {
     const size = e.target.value
-    this.setState({ smallActive: !this.state.smallActive, mediumActive: false, largeActive: false, xlargeActive: false })
+    this.setState({ xsmallActive: false, smallActive: !this.state.smallActive, mediumActive: false, largeActive: false, xlargeActive: false, xxlargeActive: false })
     this.handleSizeChange(size)
   }
 
   handleSetMediumActive(e) {
     const size = e.target.value
-    this.setState({ smallActive: false, mediumActive: !this.state.mediumActive, largeActive: false, xlargeActive: false })
+    this.setState({ xsmallActive: false, smallActive: false, mediumActive: !this.state.mediumActive, largeActive: false, xlargeActive: false, xxlargeActive: false })
     this.handleSizeChange(size)
   }
 
   handleSetLargeActive(e) {
     const size = e.target.value
-    this.setState({ smallActive: false, mediumActive: false, largeActive: !this.state.largeActive, xlargeActive: false })
+    this.setState({ xsmallActive: false, smallActive: false, mediumActive: false, largeActive: !this.state.largeActive, xlargeActive: false, xxlargeActive: false })
     this.handleSizeChange(size)
   }
 
   handleSetXlargeActive(e) {
     const size = e.target.value
-    this.setState({ smallActive: false, mediumActive: false, largeActive: false, xlargeActive: !this.state.xlargeActive })
+    this.setState({ xsmallActive: false, smallActive: false, mediumActive: false, largeActive: false, xlargeActive: !this.state.xlargeActive, xxlargeActive: false })
     this.handleSizeChange(size)
   }
 
+  handleSetXxlargeActive(e) {
+    const size = e.target.value
+    this.setState({ xsmallActive: false, smallActive: false, mediumActive: false, largeActive: false, xlargeActive: false, xxlargeActive: !this.state.xxlargeActive })
+    this.handleSizeChange(size)
+  }
 
+  renderingColorSection(){
+    // console.log("this.state.colorOptions", this.state.product.options[0].values)
+    const colorExist = this.state.product.options[0].values
+    if(colorExist[0] === "Default Title") {
+    } else {
+      return (
+        <div>
+          <h5>Color:</h5>
+          <Select
+          options={this.state.colorOptions}
+          onChange={this.getColor}
+          />
+        </div>
+      )
+    }
+  }
+
+  // <li className={`size ${this.state.xsmallActive ? 'active' : ""}`} data-value="0" onClick={ (e)=> this.handleSetXSmallActive(e) }>XS</li>
+  // <li className={`size ${this.state.smallActive ? 'active' : ""}`} data-value="1" onClick={ (e)=> this.handleSetSmallActive(e) }>S</li>
+  // <li className={`size ${this.state.mediumActive ? 'active' : ""}`} value="2" onClick={ (e)=> this.handleSetMediumActive(e) }>M</li>
+  // <li className={`size ${this.state.largeActive ? 'active' : ""}`} value="3" onClick={ (e)=> this.handleSetLargeActive(e) }>L</li>
+  // <li className={`size ${this.state.xlargeActive ? 'active' : ""}`} value="4" onClick={ (e)=> this.handleSetXlargeActive(e) }>XL</li>
+  // <li className={`size ${this.state.xxlargeActive ? 'active' : ""}`} value="5" onClick={ (e)=> this.handleSetXxlargeActive(e) }>2XL</li>
+  renderingSizeContainer() {
+    const { sizesContainer } = this.state
+    console.log("sizesContainer-------", sizesContainer)
+
+    const smallActive = `this.state.`+ `smallActive`
+    console.log("smallActive", smallActive)
+    if(sizesContainer.length == 0) {
+      console.log("YESSS")
+      sizesContainer.map((e) => {
+        console.log('e------', e)
+        const { size, name, type } = e
+        // return(
+        //   <li className={`size ${this.state.xsmallActive ? 'active' : ""}`} data-value={size} onClick={ (e)=> this.handleSetXSmallActive(e) }>{name}</li>
+        // )
+      })
+    }
+  }
+  renderingSizeSection(){
+    const sizeExist = this.state.product.options[0].values
+    if(sizeExist[0] === "Default Title") {
+      return (
+        <div>
+          <h5>Size:</h5>
+          <p>One Size</p>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h5>Size:</h5>
+          <ul id="variants">
+            <li className={`size ${this.state.xsmallActive ? 'active' : ""}`} data-value="0" onClick={ (e)=> this.handleSetXSmallActive(e) }>XS</li>
+            <li className={`size ${this.state.smallActive ? 'active' : ""}`} data-value="1" onClick={ (e)=> this.handleSetSmallActive(e) }>S</li>
+            <li className={`size ${this.state.mediumActive ? 'active' : ""}`} value="2" onClick={ (e)=> this.handleSetMediumActive(e) }>M</li>
+            <li className={`size ${this.state.largeActive ? 'active' : ""}`} value="3" onClick={ (e)=> this.handleSetLargeActive(e) }>L</li>
+            <li className={`size ${this.state.xlargeActive ? 'active' : ""}`} value="4" onClick={ (e)=> this.handleSetXlargeActive(e) }>XL</li>
+            <li className={`size ${this.state.xxlargeActive ? 'active' : ""}`} value="5" onClick={ (e)=> this.handleSetXxlargeActive(e) }>2XL</li>
+          </ul>
+        </div>
+      )
+    }
+  }
 
 
   rendeneringRightColumn(){
-    const { title, variants } = this.state.product;
+    const { title, variants, body_html } = this.state.product;
+    const { activeIndex } = this.state
+
     if( _.isEmpty(this.state.product) === false ) {
       return(
         <Container id="rightColumn">
-          <h2>{ title }</h2>
-          <h5>$ { variants[0].price }</h5>
+          <h2 className="productTitle">{ title }</h2>
+          <h5 className="productPrice">$ { variants[0].price }</h5>
+
+          <Rating id="rating" maxRating={5} defaultRating={5} icon='heart' size='large' disabled/>
 
           {/* Colors */}
-          <h5>Color:</h5>
-          <Select
-            options={this.state.colorOptions}
-            onChange={this.getColor}
-          />
+          {this.renderingColorSection()}
+
 
           {/* Size */}
-          <h5>Size:</h5>
-          <ul id="variants">
-            <li className={`size ${this.state.smallActive ? 'active' : ""}`} data-value="0" onClick={ (e)=> this.handleSetSmallActive(e) }>S</li>
-            <li className={`size ${this.state.mediumActive ? 'active' : ""}`} value="1" onClick={ (e)=> this.handleSetMediumActive(e) }>M</li>
-            <li className={`size ${this.state.largeActive ? 'active' : ""}`} value="2" onClick={ (e)=> this.handleSetLargeActive(e) }>L</li>
-            <li className={`size ${this.state.xlargeActive ? 'active' : ""}`} value="3" onClick={ (e)=> this.handleSetXlargeActive(e) }>XL</li>
-          </ul>
+          {this.renderingSizeSection()}
+
 
           {/* Qty */}
           <h5>Qty:</h5>
@@ -163,6 +282,32 @@ class Product extends Component {
             </Button>
           </Grid.Row>
 
+          {/* Product Details */}
+          <div id="productDetails">
+            <Divider className="divider" />
+
+            <Accordion styled>
+              <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                Details
+                <Icon id="accordionIcon" name='dropdown'/>
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 0}>
+                <div dangerouslySetInnerHTML={{ __html: body_html }}>
+                </div>
+              </Accordion.Content>
+
+              <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+                <Icon id="accordionIcon" name='dropdown' />
+                Shipping + Returns
+              </Accordion.Title>
+              <Accordion.Content active={activeIndex === 1}>
+                <Shipping />
+              </Accordion.Content>
+            </Accordion>
+
+            <Divider className="divider" />
+          </div>
+
         </Container>
       )
     }
@@ -171,7 +316,6 @@ class Product extends Component {
   render() {
     return (
       <Container id="product">
-        <h1>Product</h1>
 
         <Grid stackable columns={2}>
           {/* Left Column */}
@@ -184,6 +328,7 @@ class Product extends Component {
             {this.rendeneringRightColumn()}
           </Grid.Column>
         </Grid>
+
       </Container>
     )
   }
