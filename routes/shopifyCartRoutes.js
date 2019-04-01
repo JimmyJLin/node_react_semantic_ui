a177eab9769e6d68bc4c17f480c710f296143856
Jimmy Lin <usmcjimmy@gmail.com>
2019-04-01 17:55:04 -0400

    created working checkout route

--- routes/shopifyCartRoutes.js
+++ routes/shopifyCartRoutes.js
@@ -8,8 +8,49 @@ module.exports = app => {
     password: keys.password
   });
 
+  const order = {
+      "email": "usmcjimmy@gmail.com",
+      "send_receipt": true,
+      "line_items": [
+        {
+          "variant_id": 33791786248,
+          "quantity": 1,
+        }
+      ]
+    }
+
+  // CREATE NEW ORDER
+  app.post('/api/shopify/order/new', async (req, res) => {
+    console.log('order----', order)
+    await shopify.order.create(order)
+      .then((data) => {
+        console.log('data', data)
+      })
+      .catch((err) => {
+        console.log('err', err);
+      })
+
+    res.send(order)
+  })
+
+  // GET ONE ORDER
+  app.post('/api/shopify/order/id', async (req, res) => {
+    console.log('order----', order)
+    const orderId = '734567563375'
+    await shopify.order.get(orderId)
+      .then((order) => {
+        console.log('data', order)
+        res.send(order)
+      })
+      .catch((err) => {
+        console.log('err', err);
+      })
+  })
+
+
+  // GET ALL ORDERS
   app.get('/api/shopify/order', async (req, res) => {
-    await shopify.checkout.list()
+    await shopify.order.list()
       .then((data) => {
         orderList = [];
         data.forEach((order) => {
