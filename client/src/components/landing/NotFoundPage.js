34367bb5da41b92005a04fb6cba3869cf5bb7a4e
Jimmy Lin <usmcjimmy@gmail.com>
2019-04-17 11:01:22 -0400

    added catch all page

--- client/src/components/landing/NotFoundPage.js
+++ client/src/components/landing/NotFoundPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import './_notFoundPage.scss'
 
 class NotFoundPage extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Page Not Found</h2>
-        <h3>Need to Add Contents</h3>
+      <div id="pageNotFound">
+        <h1>Sorry</h1>
+        <h3>something went wrong on our end</h3>
+        <h4>Please go back and try again or go to <Link className="onHoverItem" to="/">Allthingsfrenchie's home page</Link></h4>
       </div>
     )
   }
